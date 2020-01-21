effabd6ea0dfc7c989751c2cebfee293d2b7f004
Ben J Woodcroft <donttrustben@gmail.com>
2020-01-21 16:42:33 +1000

    Initial commit.

---
+++ src/clap_utils.rs
@@ -0,0 +1,31 @@
+use std::env;
+
+use clap::*;
+use log::LevelFilter;
+use env_logger::Builder;
+
+pub fn set_log_level(matches: &clap::ArgMatches, is_last: bool, program_name: &str) {
+    let mut log_level = LevelFilter::Info;
+    let mut specified = false;
+    if matches.is_present("verbose") {
+        specified = true;
+        log_level = LevelFilter::Debug;
+    }
+    if matches.is_present("quiet") {
+        specified = true;
+        log_level = LevelFilter::Error;
+    }
+    if specified || is_last {
+        let mut builder = Builder::new();
+        builder.filter_level(log_level);
+        if env::var("RUST_LOG").is_ok() {
+            builder.parse_filters(&env::var("RUST_LOG").unwrap());
+        }
+        if builder.try_init().is_err() {
+            panic!("Failed to set log level - has it been specified multiple times?")
+        }
+    }
+    if is_last {
+        info!("{} version {}", program_name, crate_version!());
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod clap_utils;
+
+extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
